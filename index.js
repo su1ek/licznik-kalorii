485fbb3835399f9661877df04507298f91d31ead
Bartek <barteksulkowski7@gmail.com>
2026-03-20 21:24:50 +0100

    Dodanie funkcji wyszukiwania produktu z najwieksza iloscia kalorii

--- index.js
+++ index.js
@@ -9,9 +9,11 @@ const lista = document.getElementById("lista");
 let totalCalories = 0;
 
 produkty.forEach(wyswietl);
+wyszukajNajwiekszy();
 
 function wyswietl(wpis){
     const nowyProdukt = document.createElement("li");
+    nowyProdukt.dataset.id = wpis.id;
     nowyProdukt.textContent = `${wpis.nazwa}: ${wpis.kcal} kcal`;
     lista.appendChild(nowyProdukt);
     totalCalories += wpis.kcal;
@@ -21,6 +23,7 @@ function wyswietl(wpis){
         this.remove();
         const index = produkty.indexOf(wpis); //ustalenie indexu zeby działał splice
         produkty.splice(index, 1);
+        wyszukajNajwiekszy();
         zapisDoLocalStorage();
         totalCalories = totalCalories - wpis.kcal;
         wszystkieKalorie.textContent = `Liczba kalorii wynosi: ${totalCalories}`;
@@ -40,6 +43,7 @@ function pobierz(){
     }
     //tworzenie obiektu produktu
     const nowyWpis = {
+        id: Date.now(),
         nazwa: produkt.value,
         kcal: wpisaneKalorie
     };
@@ -52,6 +56,7 @@ function pobierz(){
     kalorie.value = "";
 
     wyswietl(nowyWpis);
+    wyszukajNajwiekszy();
 }
 
 function resetuj(){
@@ -61,7 +66,27 @@ function resetuj(){
     wszystkieKalorie.textContent = `Liczba kalorii wynosi: ${totalCalories}`;
     lista.textContent = "";
 }
+
 function zapisDoLocalStorage(){
     localStorage.setItem("mojeProdukty", JSON.stringify(produkty)); //zapisanie tablicy pod kluczem mojeProdukty
                                                                     //zmiana z tablicy na tekst dzieki JSON.stringify
+}
+
+function wyszukajNajwiekszy(){
+    //ze wszystkich znaczników usuwamy klasę największy
+    document.querySelectorAll('li').forEach(li => li.classList.remove('najwiekszy'));
+    
+    //jesli na liscie nie ma nic to zwracamy pusto
+    if (produkty.length === 0)
+        return;
+    
+    //wyszukujemy produkt z najwieksza liczba kalorii 
+    const lider = produkty.reduce((max, obecny) => {
+        return (obecny.kcal > max.kcal) ? obecny : max;
+    })
+
+    //wyszukiwanie id lidera
+    const elementLidera = document.querySelector(`li[data-id="${lider.id}"]`);
+    if (elementLidera)
+        elementLidera.classList.add('najwiekszy');
 }
